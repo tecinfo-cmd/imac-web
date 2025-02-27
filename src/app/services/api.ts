5ac837a8de84c4efcac0fd33ba2b2d5ec7993b3f
Bianca <bianca.raiane18@gmail.com>
2025-02-27 04:43:59 -0300

    feat: :sparkles: API implementation

---
+++ src/app/services/api.ts
@@ -0,0 +1,28 @@
+export const fetchCARData = async (
+  documentType: "cpf" | "cnpj" | "carEstadual",
+  documentValue: string
+) => {
+  let formattedValue = documentValue;
+  if (documentType === "cpf" || documentType === "cnpj") {
+    formattedValue = documentValue.replace(/[^\w\s]/gi, "");
+  }
+
+  const url = `https://imac-dev-f8b98.ondigitalocean.app/imac/api/v1/elegibilidades/consulta-car?${documentType}=${formattedValue}`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(
+        "Erro ao buscar os dados. Verifique o documento informado."
+      );
+    }
+
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    console.error("Erro na requisição:", error);
+    return null;
+  }
+};
