0b6c2eb6f990580d5a7a9e8771f31cb12994f17a
Bianca <bianca.raiane18@gmail.com>
2025-05-10 01:45:19 -0300

    refactor: :recycle: saving the ID in localStorage to use on the BuyVoucher page

--- src/app/(public)/confirmation/page.tsx
+++ src/app/(public)/confirmation/page.tsx
@@ -57,6 +57,10 @@ function ConfirmationContent() {
     )
       .then((res) => {
         if (res.ok) {
+          if (id) {
+            localStorage.setItem("userIdElegibilidade", id);
+            console.log("ID salvo no localStorage:", id);
+          }
           setStatus("success");
         } else {
           setStatus("error");
