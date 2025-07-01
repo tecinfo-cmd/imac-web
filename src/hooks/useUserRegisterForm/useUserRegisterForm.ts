a26fbdb4436fd60ef7122f102aa6ae6765499953
Bianca <bianca.raiane18@gmail.com>
2025-06-30 23:03:23 -0300

    feat: :sparkles: create and integrate registration page

---
+++ src/hooks/useUserRegisterForm/useUserRegisterForm.ts
@@ -0,0 +1,98 @@
+//import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import { api } from "@/api/index";
+import { yup } from "@/config/yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { toast } from "sonner";
+
+const unmask = (value: string) => value.replace(/\D/g, "");
+
+const schema = yup.object().shape({
+  nome: yup.string().required(),
+  cpf: yup
+    .string()
+    .required()
+    .matches(
+      /^(\d{3}\.\d{3}\.\d{3}-\d{2}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2})$/,
+      "CPF inválido"
+    ),
+  rgie: yup.string().required(),
+  telefone: yup
+    .string()
+    .required()
+    .matches(/^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/, "Telefone inválido"),
+  email: yup.string().required(),
+  profissao: yup.string().required(),
+  perfil: yup.string().required(),
+  tipo: yup.string().required(),
+});
+
+export const useUserRegister = () => {
+  //const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const { reset } = form;
+
+  const onSubmit = async (data: any) => {
+    setIsLoading(true);
+
+    const roleName = data.perfil?.toLowerCase();
+    const roleData =
+      roleName === "administrativo"
+        ? { nome: "ADMINISTRATIVO", id: 1 }
+        : roleName === "analista"
+        ? { nome: "ANALISTA", id: 2 }
+        : null;
+
+    const tipoRaw = data.tipo?.toLowerCase();
+    let tipo = null;
+
+    if (tipoRaw?.includes("pessoa física") || tipoRaw === "pf") {
+      tipo = "PF";
+    } else if (tipoRaw?.includes("pessoa jurídica") || tipoRaw === "pj") {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      tipo = "PJ";
+    }
+
+    const payload = {
+      ...data,
+      nome: data.nome ?? null,
+      cpf: data.cpf ? unmask(data.cpf) : null,
+      rgie: data.rgie ? unmask(data.rgie) : null,
+      telefone: data.telefone ? unmask(data.telefone) : null,
+      email: data.email ?? null,
+      profissao: data.profissao ?? null,
+      tipo: data.tipo ?? null,
+      roles: roleData ? [roleData] : [],
+    };
+    try {
+      console.log("Payload que será enviado:", payload);
+
+      await api.post("usuario", payload);
+      toast.success("Usuário cadastrado com sucesso!", { duration: 3000 });
+      reset();
+    } catch (error: any) {
+      const errorMessage =
+        error?.response?.data?.message || error.message || "Erro desconhecido.";
+      console.error("Erro ao cadastrar usuário:", error);
+      toast.error("Erro ao cadastrar usuário. tente novamente.", {
+        duration: 3000,
+      });
+      console.log(`${errorMessage}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    ...form,
+    onSubmit,
+    isLoading,
+  };
+};
