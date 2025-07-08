aef6f2dd2c8b03a562bceaa9da6d7d0cb693f2de
Bianca <bianca.raiane18@gmail.com>
2025-07-08 14:07:43 -0300

    refactor: :recycle: update endpoint

--- src/hooks/useGetElegibilities/useGetElegibilityDetail.ts
+++ src/hooks/useGetElegibilities/useGetElegibilityDetail.ts
@@ -1,15 +1,13 @@
 import { api } from "@/api";
 import { useQuery } from "@tanstack/react-query";
 
-export const useGetElegibilityDetail = (car: string, enabled = false) => {
+export const useGetElegibilityDetail = (id: number | string | null, enabled = false) => {
   return useQuery({
-    queryKey: ["elegibility-detail", car],
+    queryKey: ["elegibility-detail", id],
     queryFn: async () => {
-      const response = await api.get("elegibilidades/solicitacoes", {
-        params: { carFederal: car },
-      });
+      const response = await api.get(`elegibilidades/buscar-por-id/${id}`);
       return response.data;
     },
-    enabled,
+    enabled: !!id && enabled,
   });
 };
