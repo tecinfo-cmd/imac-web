9ac1d4737e2fa458afda856b4a9f6f23ea80b47b
Bianca <bianca.raiane18@gmail.com>
2025-09-28 16:11:09 -0300

    feat: :sparkles: add abattoir eligibility hooks and CPF mask utility; update abattoir registration to use FormData

--- src/hooks/useAbattoirElegibilities/useAbattoirElegibilities.ts
+++ src/hooks/useAbattoirElegibilities/useAbattoirElegibilities.ts
@@ -3,10 +3,24 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 
 const API_URL = `${process.env.NEXT_PUBLIC_API_HOST}frigoficos`;
 
-export function useAbattoirElegibilities(filters = {}, page = 1) {
+export function useAbattoirUser() {
+  return useQuery({
+    queryKey: ["abattoir-user"],
+    queryFn: async () => {
+      const { data } = await api.get(`${API_URL}/usuario`);
+      return data;
+    },
+  });
+}
+
+export function useAbattoirElegibilities(
+  filters = {},
+  page = 1,
+  refreshKey = 0
+) {
   const limit = 10;
   return useQuery({
-    queryKey: ["elegibilities", page, filters],
+    queryKey: ["elegibilities", page, filters, refreshKey],
     queryFn: async () => {
       const { data } = await api.get(`${API_URL}/elegibilidades`, {
         params: { page, size: limit, ...filters },
@@ -26,6 +40,16 @@ export function useCheckElegibility(options: any = {}) {
   return useMutation({
     mutationFn: async (numeroCar: string) => {
       const { data } = await api.get(`${API_URL}/elegibilidade/${numeroCar}`);
+      return data;
+    },
+    ...options,
+  });
+}
+
+export function useCreateProdutor(options: any = {}) {
+  return useMutation({
+    mutationFn: async (produtorData: any) => {
+      const { data } = await api.post(`${API_URL}/produtor`, produtorData);
       return data;
     },
     ...options,
--- src/utils/maskCPF.ts
+++ src/utils/maskCPF.ts
@@ -1,6 +1,7 @@
 export const maskCPF = (value: string) => {
   return value
     .replace(/\D/g, "")
+    .slice(0, 11)
     .replace(/(\d{3})(\d)/, "$1.$2")
     .replace(/(\d{3})(\d)/, "$1.$2")
     .replace(/(\d{3})(\d{1,2})$/, "$1-$2");
