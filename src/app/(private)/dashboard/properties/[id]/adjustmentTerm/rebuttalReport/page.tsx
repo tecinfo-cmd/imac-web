3ea531af69f07131f11c857535f4bf5be78649b1
Bianca <bianca.raiane18@gmail.com>
2025-10-15 22:30:18 -0300

    chore: :broom: delete rebuttal page as it will not be used

--- src/app/(private)/dashboard/properties/[id]/adjustmentTerm/rebuttalReport/page.tsx
+++
@@ -1,5 +0,0 @@
-import { RebuttalReportLayout } from "@/layouts/DashboardUser/Properties/[id]/AdjustmentTerm/RebuttalReport";
-
-export default function RebuttalReport () {
-    return <RebuttalReportLayout />;
-};
