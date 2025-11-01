6cd4c75a6de29aade79097f66d7091f9e1f38c58
Bianca <bianca.raiane18@gmail.com>
2025-11-01 19:54:53 -0300

    refactor: :recycle: Update data from the confirmation page

--- src/app/(public)/confirmation/page.tsx
+++ src/app/(public)/confirmation/page.tsx
@@ -2,16 +2,13 @@
 
 import { useSearchParams } from "next/navigation";
 import { Suspense, useEffect, useState } from "react";
-import { BsFileText } from "react-icons/bs";
-import { TbLeaf } from "react-icons/tb";
-import { TfiBookmarkAlt } from "react-icons/tfi";
 
 import { Card } from "@/components/ui/card";
 import Footer from "@/components/ui/footer";
 import Header from "@/components/ui/header";
-import Step from "@/components/ui/step";
 
 import { Logo } from "@/icons/Logo";
+import { LogoSideName } from "@/icons/LogoSideName";
 
 export default function Confirmation() {
   return (
@@ -79,36 +76,35 @@ function ConfirmationContent() {
 
   return (
     <div className="flex flex-col items-center justify-center p-10 md:p-20 gap-8 md:gap-16 w-full max-w-4xl">
-      <Logo width={130} height={130} />
-      <div className="flex flex-col self-start gap-6 md:gap-10">
+      <LogoSideName width={400} height={130} />
+      <div className="flex flex-col items-center gap-6 md:gap-10">
         <h1 className="text-left text-xl md:text-2xl text-[#1F3F13] font-bold">
-          Seu e-mail foi confirmado!
+          Seu e-mail foi confirmado com sucesso!
         </h1>
-        <h1 className="text-left text-xl md:text-2xl text-[#1F3F13] font-bold">
-          Agora vamos para a Etapa 2 - Análise socioambiental!
-        </h1>
+        <h3 className="text-left text-base md:text-lg text-[#2F3F13] font-bold">
+          Sua propriedade já está em análise! Fique tranquilo(a), você receberá
+          um novo e-mail com o resultado da consulta de Elegibilidade em até 24
+          horas.
+        </h3>
       </div>
-
-      <Card className="w-full max-w-2xl h-auto p-6 md:p-8">
-        <h2 className="text-lg md:text-xl font-bold text-center text-gray-800 border-b pb-2 mb-4">
-          Etapas do credenciamento
-        </h2>
-        <Step
-          icon={<BsFileText className="w-6 h-6 text-white" />}
-          title="Preenchimento de formulário"
-          description="Preencha o formulário de consulta informando o número CAR da propriedade ou, alternativamente, seu CPF/CNPJ e a localização da fazenda. Informe também o seu contato para receber o retorno."
-        />
-        <Step
-          icon={<TbLeaf className="w-6 h-6 text-white" />}
-          title="Análise socioambiental"
-          description="O IMAC realizará uma análise socioambiental da propriedade e informará se ela está elegível para participar da reinserção no mercado."
-        />
-        <Step
-          icon={<TfiBookmarkAlt className="w-6 h-6 text-white" />}
-          title="Credenciamento"
-          description="Se a sua propriedade for elegível, você receberá as instruções para seguir com o credenciamento no PREM."
-        />
-      </Card>
+      <div>
+        <h3 className="text-left text-base md:text-lg text-[#2F3F13] font-bold">
+          Saiba mais...
+        </h3>
+        <Card className="w-full max-w-2xl h-auto p-6 md:p-8 pt!-0">
+          <h2 className="text-lg md:text-xl font-bold text-center mb-4">
+            Entenda as 12 Etapas do Programa de Reinserção e Monitoramento.
+          </h2>
+          <div className="flex justify-center">
+            <Logo width={130} height={130} />
+          </div>
+          <div className="flex justify-center">
+            <p className="text-left text-base md:text-lg text-[#2F3F13]">
+              Programa de <br /> Reinserção e <br /> Monitoramento
+            </p>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 }
