256726fc10802b78079c44af888550b0ce262f52
Bianca <bianca.raiane18@gmail.com>
2025-11-10 16:47:03 -0300

    feat: :sparkles: create car review moked page

--- src/layouts/DashboardUser/Properties/[id]/CarReview/index.tsx
+++ src/layouts/DashboardUser/Properties/[id]/CarReview/index.tsx
@@ -1,39 +1,35 @@
 "use client";
-import { MdEngineering } from "react-icons/md";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { GoArrowLeft } from "react-icons/go";
+import { MdEngineering, MdOutlineEdit } from "react-icons/md";
 import {
-  PiFarmLight,
-  PiSealCheckLight,
   PiUserCircleThin,
+  PiSealCheckLight,
+  PiFarmLight,
 } from "react-icons/pi";
 
+import { Modal } from "../SelfInspection/components/Modal";
+import { Input } from "@/components/Input";
 import { LayoutContainer } from "@/components/LayoutContainer";
 import { Table } from "@/components/Table";
+import { Tooltip } from "@/components/Tooltip";
+import { Button } from "@/components/ui/button";
 
 import { Abattoir } from "@/icons/Abattoir";
 import { Analityc } from "@/icons/Analityc";
-import { DownloadIcon } from "@/icons/Download";
+import { maskDate } from "@/utils/maskDate";
 
-const arquivos = [
-  {
-    id: 1,
-    dataUpload: "12/09/2025",
-    nome: "documento_car_1.pdf",
-    url: "https://exemplo.com/documento_car_1.pdf",
-  },
-  {
-    id: 2,
-    dataUpload: "10/09/2025",
-    nome: "documento_car_2.pdf",
-    url: "https://exemplo.com/documento_car_2.pdf",
-  },
+const mockDocuments = [
+  { date: "06/06/2025", name: "car.pdf" },
+  { date: "06/06/2025", name: "autorizacaodesupressao.pdf" },
+  { date: "06/06/2025", name: "art.pdf" },
+  { date: "06/06/2025", name: "laudocontestacao.pdf" },
 ];
 
 const menuItems = [
-  {
-    label: "Home",
-    href: "/dashboard",
-    icon: <Analityc />,
-  },
+  { label: "Dashboard", href: "/dashboard", icon: <Analityc /> },
   {
     label: "Usuários",
     href: "/dashboard/users",
@@ -49,13 +45,6 @@ const menuItems = [
     href: "/dashboard/properties",
     icon: <PiFarmLight size={44} />,
   },
-  /*
-    {
-      label: "Multas",
-      href: "/multas",
-      icon: <Taxa />,
-    },
-    */
   {
     label: "Frigorificos",
     href: "/dashboard/abattoir-industry",
@@ -69,37 +58,126 @@ const menuItems = [
 ];
 
 export const CarReviewLayout = () => {
-  const handleDownload = (url: string) => {
-    window.open(url, "_blank");
+  const params = useParams();
+  const router = useRouter();
+  const propriedadeId = params?.id as string;
+  const [openModal, setOpenModal] = useState(false);
+  const { control, handleSubmit, reset } = useForm();
+
+  const onSubmit = () => {
+    setOpenModal(false);
+    reset();
   };
 
   return (
-    <LayoutContainer title="Revisão de Car" menuItems={menuItems}>
-      <h1 className="text-center text-[#21801A] font-bold mb-4">
-        Revisão de car
-      </h1>
-      <div className="bg-[#21801A] text-white px-4 py-2 font-semibold mt-1">
-        Proprietário Principal
+    <LayoutContainer
+      title="Revisão do Car e Outros documentos"
+      menuItems={menuItems}
+    >
+      <div className="max-w-6xl mx-auto my-8">
+        <button
+          onClick={() => router.push(`/dashboard/properties/${propriedadeId}`)}
+          className="text-[#21801A] flex items-center gap-3"
+        >
+          <GoArrowLeft size={28} />
+        </button>
       </div>
-      <Table.Container className="!pt-0">
+      <div className="flex justify-between items-center mb-8">
+        <h2 className="text-2xl font-semibold text-[#21801A] text-center w-full">
+          Revisão do Car e Outros documentos
+        </h2>
+        <Button variant="green" onClick={() => setOpenModal(true)}>
+          Novo
+        </Button>
+      </div>
+      <Table.Container>
         <Table.Header>
-          <Table.Cell>Data de upload</Table.Cell>
-          <Table.Cell colspan={2}>Nome do arquivo</Table.Cell>
+          <Table.Title
+            colspan={3}
+            className="bg-[#21801A] text-white text-base"
+          >
+            Revisão do Car e outros documentos
+          </Table.Title>
+        </Table.Header>
+        <Table.Header>
+          <Table.Title className="bg-[#D6EAD3] text-[#21801A] font-bold">
+            Data de upload
+          </Table.Title>
+          <Table.Title
+            className="bg-[#D6EAD3] text-[#21801A] font-bold"
+            colspan={2}
+          >
+            Nome do arquivo
+          </Table.Title>
         </Table.Header>
         <Table.Body>
-          {arquivos.map((arquivo) => (
-            <Table.Row key={arquivo.id}>
-              <Table.Cell>{arquivo.dataUpload}</Table.Cell>
-              <Table.Cell>{arquivo.nome}</Table.Cell>
+          {mockDocuments.map((doc, i) => (
+            <Table.Row key={doc.name + i}>
+              <Table.Cell>{doc.date}</Table.Cell>
+              <Table.Cell>{doc.name}</Table.Cell>
               <Table.Cell>
-                <button onClick={() => handleDownload(arquivo.url)}>
-                  <DownloadIcon />
-                </button>
+                <Tooltip message="Editar documento" id={`edit-${i}`}>
+                  <button
+                    className="border-2 border-[#CAC4D0] p-1 rounded"
+                    type="button"
+                  >
+                    <MdOutlineEdit size={20} color="#CAC4D0" />
+                  </button>
+                </Tooltip>
               </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
       </Table.Container>
+      <div className="mt-10">
+        <a href="#" className="text-[#21801A] underline text-sm">
+          Voltar
+        </a>
+      </div>
+
+      <Modal.Container
+        isOpen={openModal}
+        onClose={() => setOpenModal(false)}
+        className="border-1 rounded-lg shadow-sm"
+      >
+        <Modal.Header>
+          Revisão do Car e Outros documentos
+          <Modal.CloseButton onClose={() => setOpenModal(false)} />
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              name="nomeArquivo"
+              label="Nome do Arquivo"
+              placeholder="Digite o título do arquivo"
+              control={control}
+            />
+            <Input
+              name="descricaoArquivo"
+              label="Descrição do Arquivo"
+              placeholder="Digite o título do arquivo"
+              control={control}
+            />
+            <Input
+              placeholder="arraste / selecione o arquivo aqui"
+              name="file"
+              label="Insira o Arquivo"
+              control={control}
+            />
+
+            <Input
+              name="dataUpload"
+              label="Data de Upload"
+              placeholder="00/00/0000"
+              control={control}
+              mask={maskDate}
+            />
+            <Button type="submit" variant="green" className="mt-4 w-40 mx-auto">
+              Salvar
+            </Button>
+          </form>
+        </Modal.Body>
+      </Modal.Container>
     </LayoutContainer>
   );
 };
